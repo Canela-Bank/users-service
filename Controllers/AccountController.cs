02d1bb7cb80df067a6edf1892ef8a413f4fc56cb
juan rincon <juanhit471523@gmail.com>
2022-10-30 14:26:06 -0500

    crear usuario

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Canela.Service.UserMgmt.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Canela.Service.UserMgmt.Controllers
+{
+    [ApiController]
+    [Route("user")]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Create")]
+        public dynamic CrearCuenta(User usuario)
+        {
+            //llamado al integrador de datos para solicitar la creacion del usuario
+            return new
+            {
+                status = 201,
+                message= "User create",
+                result = usuario
+            };
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Canela.Service.UserMgmt.Models
+{
+    public class User
+    {
+        public string Documento { get; set; }
+        public string tipo_documento { get; set; }
+        public string nombres { get; set; }
+        public string apellidos { get; set; }
+        public string fecha_nacimiento { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+        public string correo { get; set; }
+
+    }
+}
